815903a5047dd3f28806e31cd02a9f919cb05f98
jakovsch <jakov@schrama.xyz>
2022-12-02 15:31:24 +0100

    Add utility script

---
+++ aoc.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+set -eu
+
+cd "$(dirname "$0")"
+test ".env" && source $_
+
+Y=${YEAR:-$(date +%Y)}
+D=${DAY:-$(date +%-d)}
+C="session=${COOKIE:?Missing session cookie.}"
+HEAD=$(cat <<'EOF'
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+<meta charset="utf-8"/>
+<style>
+    body { font-family: "Ubuntu", "Segoe UI", sans-serif; line-height: 1.3em; }
+    input~span:before, .leaderboard-entry, .privboard-row { font-family: "Source Code Pro", monospace; letter-spacing: -1px; }
+    body * { text-shadow: none !important; }
+    article em { font-weight: bold; }
+    main>article, main>p, pre.calendar, form, .leaderboard-entry { width: 40em !important; margin: auto; }
+    header div { text-align: center; margin: auto; }
+    main>article+p, main>article:not(:last-of-type) { opacity: 0.5; }
+</style>
+</head>
+<body>
+EOF
+)
+
+prepend() {
+    echo -ne "$@" && cat -
+}
+
+echo "Year $Y, day $D"
+mkdir -p "./$Y/$D"
+curl \
+    -sSL \
+    -b "$C" \
+    "https://adventofcode.com/$Y/day/$D/input" \
+    -o "./$Y/$D/input.txt"
+curl \
+    -sSL \
+    -b "$C" \
+    "https://adventofcode.com/$Y/day/$D" \
+    | sed -n '/<main>/,$p' \
+    | prepend "$HEAD\n" \
+    | sed '/\/article/,/\/body/{//!d}' \
+    > "./$Y/$D/problem.html"
